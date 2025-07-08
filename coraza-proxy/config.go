432843d042d30c92fc47ec998859c4da2b8befa6
Nelly Delessy <ndelessy@mdc.edu>
2025-07-08 16:41:39 -0400

    Modifying dataset capture

--- coraza-proxy/config.go
+++ coraza-proxy/config.go
@@ -4,32 +4,26 @@ import (
 	"embed"
 )
 
+// Custom log file
+const loggerFormat = "csv"
+const loggerPath = "/var/log/coraza/coraza-audit-dta.csv"
+
+// Default values for AI verdict and vulnerability type labels.
+// These can be modified here without touching the logger logic.
+const (
+	DefaultAIVerdictLabel           = "malicious" //benign or malicious
+	DefaultAIVulnerabilityTypeLabel = "dta"       //none, sqli, xss, dta, etc
+)
 const (
 	aiMicroserviceURL = "http://ai-microservice:5000/classify"
 	targetAppURL      = "http://juice-shop:3000"
 )
 
-
-
-//Custom Directives for Coraza
+// Custom Directives for Coraza
 const customCorazaPath = "/etc/coraza/coraza.conf"
 
-//Custom log file
-// The path /var/log/coraza/coraza-audit.log needs write permissions for the user running the app.
-// Consider making this configurable via env var or flag.
-const loggerFormat = "csv"
-const loggerPath ="/var/log/coraza/coraza-audit-benign.csv"
-// Default values for AI verdict and vulnerability type labels.
-// These can be modified here without touching the logger logic.
-const (
-	DefaultAIVerdictLabel         = "benign" //benign or malicious
-	DefaultAIVulnerabilityTypeLabel = "benign"    //none, sqli, xss, etc
-)
-
 const wazuhLoggerFormat = "json"
-const wazuhLoggerPath ="/var/log/coraza/coraza-audit.json"
-
-
+const wazuhLoggerPath = "/var/log/coraza/coraza-audit.json"
 
 // The annotation below embeds the CRS rules in the "owasp-crs-v4/"" folder
 //
@@ -47,4 +41,3 @@ func GetAIMicroserviceURL() string {
 func GetTargetAppURL() string {
 	return targetAppURL
 }
-
